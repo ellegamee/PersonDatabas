8b489e89c9184ed4090d68d5b4542e62e25946a3
Elias Axonov <ed.axonov@gmail.com>
2022-01-27 03:35:13 +0100

    lot of more functionality with being able to add multiple persons and update their name etc.

--- run.py
+++ run.py
@@ -25,22 +25,36 @@ class Person():
         elif index == 3:
             self.id_number = value          
     
-def user_management(values, bool):
-    listbox_selcted = listbox.get(listbox.curselection())
-    # Update already created person
-    if bool == True:
-        pass
-    
-    # If new person is created
-    elif bool != True and listbox_selcted == 'Add Person +':
-        users.append(Person(root))
-        for index, value in enumerate(values):
-            users[-1].update_info(value, index)
+def user_management(values):
+    if listbox.curselection() != "":
+        listbox_selcted = listbox.get(listbox.curselection())
         
-        #print('name', users[-1].name)
-        listbox.insert(0, users[-1].name)
+        # If new person is created
+        if listbox_selcted == 'Add Person +':
+            users.append(Person(root))
+            for index, value in enumerate(values):
+                users[-1].update_info(value, index)
+            
+            listbox.insert(0, users[-1].name)
+            
+            # Selected newly created person
+            listbox.selection_clear(0, END)
+            listbox.selection_set(0)
 
-def entry_information():
+        # Update already created person
+        else:
+            for index, value in enumerate(values):
+                users[listbox.curselection()[0]].update_info(value, index)
+            
+            set_entry_information(listbox.curselection()[0])
+
+def set_entry_information(selected_index):        
+    listbox.delete(selected_index)
+    listbox.insert(selected_index, users[selected_index].name)
+    listbox.selection_clear(0, END)
+    listbox.selection_set(selected_index)
+
+def get_entry_information():
     lst = []    
     for entry in entrys:
         lst.append(entry.get())
@@ -70,7 +84,7 @@ root.geometry(f'{width}x{height}+{left_padding}+{top_padding}')
 users = []
 
 # Listbox
-listbox = Listbox(root)
+listbox = Listbox(root, exportselection=False)
 listbox.place(anchor="center" ,relx=0.1, rely=0.5, relheight=0.9, relwidth=0.15)
 
 # Add person choice
@@ -95,7 +109,7 @@ for index in range(len(label_text)):
 
 # Save and Cancel
 button_frame = Frame(root)
-Button(button_frame, text="Save", command=lambda : (user_management(entry_information(), False))).pack()
+Button(button_frame, text="Save", command=lambda : (user_management(get_entry_information()))).pack()
 Button(button_frame, text="Cancel").pack()
 
 button_frame.pack()
